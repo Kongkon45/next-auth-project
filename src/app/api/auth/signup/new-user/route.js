95a49c25ea8709528af1764feb10a3e61636eb52
Kongkon Jowarder <kongkon4545@gmail.com>
2024-10-11 21:30:18 +0600

    password secure for bcrypt npm package use

--- src/app/api/auth/signup/new-user/route.js
+++ src/app/api/auth/signup/new-user/route.js
@@ -1,10 +1,13 @@
 import connectDB from "@/components/lib/connectDB"
+import bcrypt from 'bcrypt'; 
 
 export const POST = async (request)=>{
     try {
         const db = await connectDB();
         const userCollection = db.collection("users-info");
         const newUser = await request.json();
+        const handlePassword = await bcrypt.hash(newUser.password, 10)
+        newUser.password = handlePassword
         const res = await userCollection.insertOne(newUser);
         console.log(res);
         return Response.json({message : "new user create"})
